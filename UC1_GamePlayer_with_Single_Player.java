de2dc68dc68aed19bb66ddf04b7a7b341e9d9c43
Poonam Mane <manepoonam001@gmail.com>
2023-02-06 08:26:48 +0530

    Welcome

---
+++ UC1_GamePlayer_with_Single_Player.java
@@ -0,0 +1,12 @@
+package com.day4_SnakeandLadder;
+
+public class UC1_GamePlayer_with_Single_Player {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder Program");
+		int playerPosition=0;
+		System.out.println("Position of the player before game start position : "+playerPosition);
+
+	}
+
+}
