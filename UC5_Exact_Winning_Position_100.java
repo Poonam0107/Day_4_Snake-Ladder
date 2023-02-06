42d4933e1bf3e90405e7ef26b1981f2665260f9d
Poonam0107 <120553133+Poonam0107@users.noreply.github.com>
2023-02-06 08:48:36 +0530

    Exact Winning Position 100

---
+++ UC5_Exact_Winning_Position_100.java
@@ -0,0 +1,58 @@
+package com.day4_SnakeandLadder;
+
+public class UC5_Exact_Winning_Position_100 {
+
+	public static final int WINNING_POSITION = 100;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder Program");
+		int playerPosition=0;
+		int i=0;
+		System.out.println("Position of the player before game start position : "+playerPosition);
+		while (playerPosition !=WINNING_POSITION)
+		{
+			System.out.println("Roll Dice to Move");
+			int dice =(int) (Math.floor((Math.random()*6)+1));
+			System.out.println("The Dice is :"+dice);
+			int chance =(int) Math.floor(Math.random()*3);
+			if(chance==0) 
+			{
+				System.out.println("No Play");
+				playerPosition=playerPosition;
+				i++;
+			}
+			else if(chance==1)
+			{
+				System.out.println("Got a Ladder");
+				if(playerPosition<WINNING_POSITION)
+				{
+					playerPosition=playerPosition +dice;
+					i++;
+					if(playerPosition>WINNING_POSITION)
+					{
+						System.out.println("position getting more than 100");
+						playerPosition=playerPosition-dice;
+					}
+					}
+				else
+				{
+					playerPosition=playerPosition;
+					i++;
+				}
+		  }
+			else
+			{
+				System.out.println("Hit with snake");
+				playerPosition=playerPosition-dice;
+				if(playerPosition <0)
+				{
+					playerPosition=0;
+				}
+			}
+			System.out.println("player present position is "+playerPosition);
+			System.out.println("Moves : "+i);
+			
+		}
+	}
+
+}
